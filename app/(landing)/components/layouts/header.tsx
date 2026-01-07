d8224563b6377d73ec846e927bf7eb7ea418e33d
gbsf7 <3332200014@untirta.ac.id>
2026-01-07 12:01:57 +0700

    trying header stuff

---
+++ app/(landing)/components/layouts/header.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FiSearch, FiShoppingBag } from "react-icons/fi";
+
+const Header = () => {
+    return (
+        <header>
+            <p>This is a head</p>
+        </header>
+    )
+}
+
+export default Header;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-primary">
+      <h1>Isit poppin yet?!</h1>
+    </main>
+  );
+}
